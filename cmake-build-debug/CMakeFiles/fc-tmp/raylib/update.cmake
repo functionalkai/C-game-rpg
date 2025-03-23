0ba045318e17af60fd7db7113e03f55d08efb765
Kai <kieranwrightva@gmail.com>
2025-03-23 19:44:50 +1000

    raylib works + movement and mouse

---
+++ cmake-build-debug/CMakeFiles/fc-tmp/raylib/update.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION 3.29)
+
+message(VERBOSE "Executing update step for raylib")
+
+block(SCOPE_FOR VARIABLES)
+
+include("C:/Users/doom3/CLionProjects/alpha_game_build_vastaria/cmake-build-debug/CMakeFiles/fc-tmp/raylib/raylib-gitupdate.cmake")
+
+endblock()
